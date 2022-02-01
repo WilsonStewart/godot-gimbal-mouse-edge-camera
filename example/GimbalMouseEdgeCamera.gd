06d89bd7bdb9e7cae19816ea085c37df19f1f3a4
Wilson Stewart <wilson@creativemachine.org>
2022-01-31 17:46:54 -0700

    started on gimbal code again

--- example/GimbalMouseEdgeCamera.gd
+++ example/GimbalMouseEdgeCamera.gd
@@ -1,3 +1,29 @@
 extends Spatial
 
+# mouse
+var mouse_position_h
+var mouse_position_v
 
+# rotation
+var rotation_h
+var rotation_v
+var rotation_inertia_h
+var rotation_inertia_v
+var doRotate_h = false
+var doRotate_v = false
+
+func _input(event):
+    if event is InputEventMouseMotion:
+        update_mouse_position(event)
+    
+    # close game on clicking escape #DEBUG 
+    if Input.is_key_pressed(KEY_ESCAPE):
+        get_tree().quit()
+
+func _process(delta):
+    pass
+
+# update the mouse position every time motion is detected
+func update_mouse_position(e):
+    mouse_position_h = e.position.x
+    mouse_position_v = e.position.y
